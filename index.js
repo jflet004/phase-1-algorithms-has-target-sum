a4be299c8d79655e538acb81c7136610f9c9ddaf
Jose Fletes <joseangelfletes@gmail.com>
2022-03-15 08:43:42 -0700

    commit tree b172dcf7326cc63a21d664e7174301f9a0a3d628

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
+/*
+  Rewrite the Problem in your Own Words here:
+
+  Return true if the sum of any pair of numbers inside array equals to the target number.
+*/
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      const getSum = array[i] + array[j];
+      if(getSum === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here:
+
+  Quadratic 0(n^2) - needs optimization!
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+
+  for every pair of numbers in the array
+    if the sum of two equal to the target
+      return true.
 */
 
 /*
   Add written explanation of your solution here
+  I used a couple of for loops to iterate through the array two numbers at a time, then used an if statement to return true if the sum of two of those numbers equal the target number.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('');
+
+  console.log('Expecting: false');
+  console.log('=>', hasTargetSum([7, 2, 8, 3, 6, 9], 21));
+  
+  console.log('');
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
